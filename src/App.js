b15d21bdc81fae8899cc0a2db32e26eb37aae22f
revathip2023 <revathips2023@gmail.com>
2023-05-14 23:38:00 +0530

    using state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import Component1 from './Component1'
@@ -6,13 +6,19 @@ import Component2 from './Component2'
 import Component3 from './Component3'
 
 export default function App() {
+  const [profession, setprofession] = useState('props')
+  
   return (
     <div>
       <Header name={'Revathi'} place={'Bangalore'}/>
-      <h1>Main App</h1>
+      <h1>Main App {profession}</h1>
       <Component1 name={'React'} classAtended={2}/>
       <Component2 number={1} topic={'Git Hub Deployment'}/>
       <Component3/>
+      <button onClick={()=>{setprofession('state')}}>
+        Profession
+      </button>
+
       <Footer/>
     </div>
   )
